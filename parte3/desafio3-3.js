4a658ffecec4d750fa21af909bffbf92cdbfbf1a
Leonardo <leonardo-f98@hotmail.com>
2023-03-16 11:25:16 -0300

    Exercício 3.3 Finalizado

--- parte3/desafio3-3.js
+++ parte3/desafio3-3.js
@@ -1,5 +1,17 @@
-let offset = 7;
-let limit = 9;
+/*Vocês decidem que vão enviar projetos e ideias para a Oil Corp seguir sendo mais sustentável. Uma pessoa do seu grupo diz que vocês receberam um JSON com diversos funcionários da Oil Corp, e seus emails, e que vocês deveriam tentar enviar as ideias para esses endereços. Porém, vocês devem poupar esforços e enviar esses emails somente para cargos de liderança e diretoria.
+
+Crie um código que, faça a analise do JSON procurando por cargos e emails válidos, e print no console o nome, o cargo e seu email válido.
+
+Caso não o algoritmo não encontre nenhum cargo e emprego válido, print no console "Não existem registros encontrados".
+
+OBS: Emails válidos são aqueles que possuem @oilcorp.com.br completo.*/
+
+//Desafios JavaScript na DIO têm funções "gets" e "print" acessíveis globalmente:
+//- "gets" : lê UMA linha com dado(s) de entrada (inputs) do usuário;
+//- "print": imprime um texto de saída (output), pulando linha.
+
+let offset = parseInt(gets());
+let limit = parseInt(gets());
 
 const info = [
   {
@@ -79,7 +91,10 @@ const findItem = (object, index) =>
   Object.keys(object).filter((item) => item.toString() == index);
 
 let arrayRaw = [];
-for (let i = 1; i <= limit - offset; i++) {
+if (offset == 0) {
+  offset = 1;
+}
+for (let i = 0; i <= limit - offset; i++) {
   let resultado = info[findItem(info, offset + i)];
   if (resultado) {
     arrayRaw.push(resultado);
@@ -92,12 +107,11 @@ const lideres = arrayRaw.filter(filtraLider);
 
 function filtraLider(value) {
   let emailTeste = value.email;
-  let finalEmail = "@oilcorp.com.br";
-  if (
-    value.cargo == "Liderança" ||
-    (value.cargo == "Diretoria" && emailTeste.includes(finalEmail))
-  ) {
-    return value;
+  let check = /^[\w.%+-]+@oilcorp\.com\.br$/.test(emailTeste);
+  if (value.cargo == "Liderança" || value.cargo == "Diretoria") {
+    if (check === true) {
+      return value;
+    }
   }
 }
 
@@ -114,11 +128,9 @@ function mapearInformacao(lider) {
 const lideresMapeados = lideres.map(mapearInformacao);
 
 if (lideres.length === 0) {
-  console.log("Não existem registros encontrados");
+  print("Não existem registros encontrados");
 } else {
   lideresMapeados.forEach(function (lider) {
-    console.log(
-      `Nome: ${lider.nome}, Cargo: ${lider.cargo}, Email: ${lider.email}`
-    );
+    print(`Nome: ${lider.nome}, Cargo: ${lider.cargo}, Email: ${lider.email}`);
   });
 }
